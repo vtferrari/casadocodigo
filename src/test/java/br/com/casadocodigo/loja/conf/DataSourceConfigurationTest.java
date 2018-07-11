973b34fba0eb252e0b50002aaaacaa3caa17187b
vtferrari <vtferrari@hotmail.com>
2018-07-10 21:06:53 -0300

    Correção da config de testes

--- src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
+++ src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
@@ -7,17 +7,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import java.util.Properties;
+
 public class DataSourceConfigurationTest {
 
 	@Bean
 	@Profile("test")
 	public DataSource dataSource(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo_test");
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false");
+		dataSource.setDriverClassName("org.h2.Driver");
 		dataSource.setUsername("root");
 		dataSource.setPassword("");
 		return dataSource;
+	}
+	@Bean
+	@Profile("test")
+	public Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		//propriedade para mostra o DDL gerado como log
+		properties.setProperty("javax.persistence.schema-generation.scripts.create-target", "db-schema.jpa.ddl");
+		return properties;
 	}
 
 }
